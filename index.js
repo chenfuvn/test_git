31eada3e1c2c9c73be01cf12cff28af8b15acd56
phuc.tran <phuc.tran@kyanon.digital>
2023-02-07 15:01:43 +0700

    edit

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.get("/", (req, res)=> {
         message: "OK"
     });
 })
+app.get("/hello", (req, res)=> {
+    return res.json({
+        status: true,
+        message: "Hello"
+    });
+})
 
 app.listen("4567", ()=>{
     console.log("server is running in localhost: 4567");
